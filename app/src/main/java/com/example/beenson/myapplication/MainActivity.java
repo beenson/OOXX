dedbbc8007d3cc560a34e51fb59d6fde33b44d26
beenson <benson070707@yahoo.com.tw>
2017-04-30 22:28:49 +0800

    OXOX

--- app/src/main/java/com/example/beenson/myapplication/MainActivity.java
+++ app/src/main/java/com/example/beenson/myapplication/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.beenson.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -13,30 +10,17 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-int j=0;
+
+    int j = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
-//        for(int i = 1;i<=9;i++) {
-//            for(int j = 1;j <=9;j++) {
-//                TextView txt = new TextView(this);
-//                txt.setText(i + " X " + j + " = " + i*j);
-//                txt.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
-//                LinearLayout.LayoutParams layout_966 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-//                int pixels = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
-//                layout_966.setMargins(pixels, pixels, pixels, pixels);
-//                txt.setLayoutParams(layout_966);
-//                linearLayout.addView(txt);
-//
-//                Log.d("output", i + " X " + j + " = " + i*j);
-//            }
-//        }
-
-        ArrayList<TextView> txtGrids = new ArrayList<>();
+        final ArrayList<TextView> txtGrids = new ArrayList<>();
+        ArrayList<TextView> OOXX = new ArrayList<>();
         for(int i=1;i<10;i++){
             TextView txtTemp = (TextView) findViewById(getResources().getIdentifier("grid"+i,"id",getPackageName()));
             txtGrids.add(txtTemp);
@@ -46,12 +30,16 @@ int j=0;
             txtGrids.get(i).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ((TextView)v).setText(j);
+                    if((j % 2 == 0) && (((TextView) v).getText() == "")) {
+                        ((TextView) v).setText("O");
+                    }
+                    else if (((TextView) v).getText() == "") {
+                        ((TextView) v).setText("X");
+                    }
+
                     j++;
-              }
+                }
             });
         }
-
-
     }
 }
